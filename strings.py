c9719979c6d4482ed5a6e99f13dd09e644775c7c
olandodeflexy <42207883+olandodeflexy@users.noreply.github.com>
2021-09-24 15:44:38 +0000

    Delete strings.py

--- strings.py
+++
@@ -1,9 +0,0 @@
-sentence = "the boy is loing to school"
-print(len(sentence))
-
-print(sentence.index("l"))
-
-new_sentence = sentence[0:11] + "coming" + sentence[38:] + " from home"
-new_sentence1 = sentence[0:12] + "g"
-print(new_sentence[21])
-print(new_sentence1)
